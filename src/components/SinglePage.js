1a5574e5f05063ea067ce1907e97df868295a996
warmeta <warmeta@gmail.com>
2018-03-05 11:49:49 +0000

    fix

--- src/components/SinglePage.js
+++ src/components/SinglePage.js
@@ -3,6 +3,10 @@ import renderHTML from 'react-render-html'
 import { Container, Row, Col } from 'reactstrap'
 
 export default class extends Component {
+  static propTypes = {
+    //
+  }
+
   render() {
     return (
       <div>
